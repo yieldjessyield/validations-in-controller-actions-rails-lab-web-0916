530a1c65a33c2f3a712e7f3316bf1c84522c96d8
Jess <jess.harrelson0707@gmail.com>
2016-10-25 16:51:42 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
+    @valid_post = Post.new(post_params)
+    if @valid_post.valid?
+      @post.update(post_params)
     redirect_to post_path(@post)
+    else
+      render :edit
+    end
+
   end
 
   private
